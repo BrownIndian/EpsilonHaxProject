39b60105bd9ea08baac8181e14573c970c38ffa4
Akshay Kumar <akshay.gkumar37949@gmail.com>
2020-06-16 18:13:40 -0700

    fix css issues and implement base.html
    Made a base.html for all the other html files to extend from and all of the css works for given file

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('checkout.html')
+    
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

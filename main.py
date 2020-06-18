680d468e3200e00b2f773410bef3cd6c7c83fa63
Akshay Kumar <akshay.gkumar37949@gmail.com>
2020-06-17 23:34:35 -0700

    fix signin

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, request, url_for
 from forms import RegisterForm, LoginForm
 from manager import Fire
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "asd2345khkgkjf7saiyd"
 
-@app.route('/', methods=['GET, POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        pass
+        return redirect(url_for('index'))
     return render_template('register.html', form=form)
     
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('signin.html', form=form)
 
 
 if __name__ == '__main__':

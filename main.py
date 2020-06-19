58411dcafb0ad68d0f9d3b8496203838058c1b13
Akshay Kumar <akshay.gkumar37949@gmail.com>
2020-06-18 19:42:22 -0700

    flaskify branch and master files

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ app.config['SECRET_KEY'] = "asd2345khkgkjf7saiyd"
 def index():
     return render_template('index.html')
 
+@app.route('/services')
+def services():
+   return render_template('services.html')
+
+@app.route('/stats')
+def stats():
+   return render_template('stats.html')
+
+@app.route('/dashboard')
+def dashboard():
+   return render_template('dashboard.html')
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('profile.html')
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
